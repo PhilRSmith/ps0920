7d2362fbc542301dc5abf3339a1328c55cfa3754
PhilRSmith <theonepudgypanda@gmail.com>
2020-09-03 15:31:39 -0400

    cleaned up code

--- src/ToolRental/RentalMain.java
+++ src/ToolRental/RentalMain.java
@@ -2,8 +2,17 @@ package toolRental;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+/**
+ * Handles the transactional portion of the program
+ * @author Philip Smith
+ * Specs created by anonymous (notme)
+ */
 public class RentalMain 
 {
+	public static void Checkout() 
+	{
+		
+	}
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException
 	{
